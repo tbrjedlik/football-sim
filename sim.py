e0d7c5d7e9f9d7903e615f91e722f9cb74b95e1a
tbrjedlik <toth.barna.robert@students.jedlik.eu>
2024-03-09 21:48:26 +0100

    győztes kiszámolása

--- sim.py
+++ sim.py
@@ -12,8 +12,10 @@ def probs_calc(home_ranking:float, away_ranking:float) -> list[float]:
     home_advantage = (random.randint(100, 120)) / (  away_ranking * (random.randint(70, 99)/100)  )
     
     expected_home = ( 1 / (1 + 10 ** ((away_ranking - home_ranking) /  random.randint(20,35)     )) ) * home_advantage
-    expected_away = 1 / (1 + 10 ** ((home_ranking - away_ranking) / random.randint(20,35)    ))
+
     expected_draw = ( 1 / (1 + 10 ** ((home_ranking - away_ranking) / 35)) ) * (home_advantage * ( random.randint(6,10) / 10 ) )
+    
+    expected_away = 1 / (1 + 10 ** ((home_ranking - away_ranking) / random.randint(20,35)    ))
 
 
 
@@ -38,3 +40,28 @@ def probs_calc(home_ranking:float, away_ranking:float) -> list[float]:
 # print(f'A {prob_to_odd(probs[2]):.2f}')
 
 
+
+def probs_to_results(probs:list[float]) -> str:
+    results = ''
+    
+    n = 5
+    
+    home = int ( round(probs[0], n) * 10**(n+1) )
+    draw = int ( round(probs[1], n) * 10**(n+1) )
+    away = int ( round(probs[2], n) * 10**(n+1) )
+    
+    sum = home + draw + away
+    
+
+    winner = random.randint(1, sum)
+    
+    print(winner)
+
+    if winner in range(1, home+1):
+        return 'H'
+    if winner in range(home+1, home+draw+1):
+        return 'D'
+    else:
+        return 'A'
+    
+    
